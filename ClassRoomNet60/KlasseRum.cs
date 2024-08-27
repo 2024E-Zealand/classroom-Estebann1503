dc3f6300defbf6f377d75b4101178bc6409792ea
Estebann1503 <144995626+Estebann1503@users.noreply.github.com>
2024-08-27 13:54:25 +0200

    Opgave 2

---
+++ ClassRoomNet60/KlasseRum.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassRoomNet60
+{
+    public class KlasseRum
+    {
+        // Properties
+        public string KlasseNavn { get; set; }
+        public List<Studerende> Klasseliste { get; set; }
+        public DateTime SemesterStart { get; set; }
+
+        // Default constructor
+        public KlasseRum()
+        {
+            Klasseliste = new List<Studerende>();
+
+        }
+    }
+}
