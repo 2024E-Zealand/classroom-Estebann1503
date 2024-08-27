96d9a097cbb8a278073245a8bf334508285ade1b
Estebann1503 <144995626+Estebann1503@users.noreply.github.com>
2024-08-27 14:13:38 +0200

    Opgave 3

--- ClassRoomNet60/Program.cs
+++ ClassRoomNet60/Program.cs
@@ -1,2 +1,26 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using ClassRoomNet60;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        // Opret en instans af KlasseRum
+        KlasseRum klasseRum = new KlasseRum
+        {
+            KlasseNavn = "3B",
+            SemesterStart = new DateTime(2022, 9, 1)
+        };
+
+        // Tilføj studerende til Klasseliste
+        Studerende student1 = new Studerende("Thomas", 3, 15);
+        Studerende student2 = new Studerende("Søren", 8, 28);
+        Studerende student3 = new Studerende("Christian", 9, 17);
+
+        klasseRum.Klasseliste.Add(student1);
+        klasseRum.Klasseliste.Add(student2);
+        klasseRum.Klasseliste.Add(student3);
+
+
+    }
+}
